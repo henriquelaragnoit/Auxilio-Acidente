e39bb8f8ba28e396876e0fc8c684fef445fecc52
henriquelaragnoit <henrique_costa93@hotmail.com>
2025-08-27 19:53:35 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-// vite.config.ts
 import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Auxilio-Acidente/', // <- nome EXATO do repo
+    base: '/Auxilio-Acidente/', // case-sensitive!
+    plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
